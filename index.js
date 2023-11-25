be2d7ef8604c668e7f21f9376bc0894d2ad792a4
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2023-11-26 01:55:57 +0600

    feature : url extract

---
+++ index.js
@@ -0,0 +1,33 @@
+// dependecies
+
+const http = require('http')
+const url = require('url')
+// the server should respond to all the request with a string .
+const server = http.createServer(function(req,res){
+
+//  get the url and parse it
+
+const parseUrl = url.parse(req.url,true)
+
+// get the path of the url
+
+const path = parseUrl.pathname;
+const trimedPath = path.replace(/^\/+|\/+$/g,'');
+
+//send the response
+res.end('hello world!')
+// log the request path
+console.log('Requested recieved untrimed path ==> ',path)
+
+
+console.log('Requested recieved trimed path ==> ',trimedPath)
+
+});
+
+// start the server and run it on 3000 port
+
+
+server.listen(3000,function(){
+    console.log('Server listening to 3000 port!')
+})
+
